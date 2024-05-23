06785906b0b9f570b20d230dfd485369976beee8
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2024-05-23 15:39:48 +0700

    feat: create ladang and active deck dinamically

--- src/main/java/org/bro/tubesoop2/DraggableItem.java
+++ src/main/java/org/bro/tubesoop2/DraggableItem.java
@@ -1,36 +1,67 @@
 package org.bro.tubesoop2;
+
+import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
+import javafx.scene.input.*;
 
 public class DraggableItem extends ImageView {
 
+    protected boolean isDefaultImage; // Change to protected to allow subclass access
+
     public DraggableItem() {
-        // Add event handlers for drag and drop
+        Image testImage = new Image(getClass().getResourceAsStream("assets/test.png"));
+        this.setImage(testImage);
+        this.isDefaultImage = false;
+
         this.setOnDragDetected(this::dragDetected);
         this.setOnDragDone(this::dragDone);
+        this.setOnDragOver(this::dragOver);
+        this.setOnDragDropped(this::dragDropped)    ;
     }
 
-    private void dragDetected(MouseEvent event) {
+    public void dragDetected(MouseEvent event) {
         ImageView sourceView = (ImageView) event.getSource();
-        Dragboard dragboard = sourceView.startDragAndDrop(TransferMode.MOVE);
 
-        ClipboardContent content = new ClipboardContent();
-        content.putImage(sourceView.getImage());
-        dragboard.setContent(content);
+        if (!isDefaultImage) {
+            Dragboard dragboard = sourceView.startDragAndDrop(TransferMode.MOVE);
+
+            ClipboardContent content = new ClipboardContent();
+            content.putImage(sourceView.getImage());
+            dragboard.setContent(content);
+        }
 
         event.consume();
     }
 
-    private void dragDone(DragEvent event) {
+    public void dragDone(DragEvent event) {
         if (event.getTransferMode() == TransferMode.MOVE) {
             ImageView sourceView = (ImageView) event.getSource();
-            sourceView.setImage(new Image("file:/assets/basic.png"));
+            Image defaultImage = new Image(getClass().getResourceAsStream("assets/basic.png"));
+            sourceView.setImage(defaultImage);
+            isDefaultImage = true;
+            System.out.println("Image set to default.");
+            if (this instanceof ProductCard) {
+                ((ProductCard) this).tes();
+            }
         }
+        event.consume();
+    }
+
+    public void dragOver(DragEvent event) {
+        if (event.getDragboard().hasImage() || event.getDragboard().hasFiles()) {
+            event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        }
+        event.consume();
+    }
+
+    public void dragDropped(DragEvent event) {
+        ImageView destinationView = (ImageView) event.getSource();
+        Dragboard dragboard = event.getDragboard();
+        if (dragboard.hasImage()) {
+            destinationView.setImage(dragboard.getImage());
+        }
+        event.setDropCompleted(true);
         event.consume();
     }
 }
---
+++ src/main/java/org/bro/tubesoop2/ProductCard.java
@@ -0,0 +1,8 @@
+package org.bro.tubesoop2;
+
+public class ProductCard extends DraggableItem{
+    public void tes(){
+        System.out.println("masuk");
+    }
+
+}
